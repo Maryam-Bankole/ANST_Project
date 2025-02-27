711e2283628ca41e3e874ceb3c8eae8119d6d637
Maryam-Bankole <maryambankole12@gmail.com>
2025-02-27 15:56:38 +0100

    Added arithmetic operations code

--- arithmetric.js
+++ arithmetric.js
@@ -1,6 +1,21 @@
+// Declare two variables and assign them any two numbers
 
 let num1 = 30;
 let num2 = 15;
 
-sum = num1+num2
-console.log(sum);
+// Perform operations and store the results in separate variables
+
+let addition = num1 + num2;
+console.log(`Addition: ${num1} + ${num2} = ${addition}`);
+
+let subtraction = num1 - num2;
+console.log(`Subtraction: ${num1} - ${num2} = ${subtraction}`);
+
+let multiplication = num1 * num2;
+console.log(`Multiplication: ${num1} * ${num2} = ${multiplication}`);
+
+let division = num1 / num2;
+console.log(`Division: ${num1} / ${num2} = ${division}`);
+
+let modulus = num1 % num2
+console.log(`Modulus: ${num1} % ${num2} = ${modulus}`);
